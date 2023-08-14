1bc08456b8fa89b6a72f4061b73c562cc5ad2d20
Kelvin <kelvin71996@hotmail.com>
2023-08-13 21:08:13 -0300

    solucion-clic

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
 document.getElementById('miDiv').addEventListener('click', function() {
     alert('Hola! Soy el div');
   });
+  window.onload = function() {
+    alert('Hola!');
+};
+function saludar(event) {
+  alert("¡Hola! Has hecho clic en el botón.");
+  event.stopPropagation(); // Detiene la propagación del evento
+}
